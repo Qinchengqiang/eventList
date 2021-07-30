2d53d1eb46a430ca059edbe14611e1f8e738c111
Qinchengqiang <steven.qin90@gmail.com>
2021-07-30 12:30:55 +1000

    update

--- window_event.js
+++ window_event.js
@@ -1,2 +1,7 @@
 
 window.addEventListener('load', (e) => console.log('page is fully loaded'));
+window.onload = (e) => console.log('page is fully loaded');
+
+const badImg = document.getElementById('bad-img');
+badImg.addEventListener('error', (e) => console.log(e));
+badImg.error = () => console.log('error');
